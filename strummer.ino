5f3925709d09710ab769f90f2c24cbb0aba3e75d
Krishnan Shankar <krishnans2006@gmail.com>
2024-02-28 14:03:19 -0500

    Parametrize strummer rotation (degrees)

--- strummer.ino
+++ strummer.ino
@@ -1,26 +1,24 @@
 #include <Stepper.h>
 
-// change this to the number of steps on your motor
-#define STEPS 200
+// Parameters
+const int rotationDegrees = 72;
 
-// create an instance of the stepper class, specifying
-// the number of steps of the motor and the pins it's
-// attached to
+#define STEPS 200
 Stepper stepper(STEPS, 4, 5, 6, 7);
 
+const int numSteps = (rotationDegrees * STEPS)/ 360;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Stepper test!");
   // setSpeed(60) apparently sets the speed of the motor to 30 RPMs
   stepper.setSpeed(30);
 }
 
 void loop() {
   Serial.println("Forward");
-  stepper.step(STEPS / 8);
+  stepper.step(numSteps);
   delay(500);
   Serial.println("Backward");
-  stepper.step(-STEPS / 8);
+  stepper.step(-numSteps);
   delay(5000);
 }
